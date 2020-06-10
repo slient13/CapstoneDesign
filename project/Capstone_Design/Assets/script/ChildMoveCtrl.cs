3b38aefa3d7e7a2fef0c0d3652ad17292693504f
Kwakhyunjong <pig9341@naver.com>
2020-06-10 22:15:45 +0900

    Player
    Player movement

---
+++ project/Capstone_Design/Assets/script/ChildMoveCtrl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildMoveCtrl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Rotate(0.0f, -90.0f * Time.deltaTime, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Rotate(0.0f, 90.0f * Time.deltaTime, 0.0f);
+        }
+    }
+}
