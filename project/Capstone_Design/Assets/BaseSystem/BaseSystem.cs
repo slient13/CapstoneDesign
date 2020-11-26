ae3913b40135f916641bd7873e4baf12c4c2181d
slient13 <sjyhc25@gmail.com>
2020-11-27 05:10:18 +0900

    플레이 정보 1차 + 업데이트 로그

--- project/Capstone_Design/Assets/BaseSystem/BaseSystem.cs
+++ project/Capstone_Design/Assets/BaseSystem/BaseSystem.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BaseSystem : MonoBehaviour
 {
     public GameObject target;
+    public List<PlayInfo> playInfoList = new List<PlayInfo>();
 
     void Start() {
     }
@@ -14,11 +15,49 @@ public class BaseSystem : MonoBehaviour
         target = GameObject.Find(msg.targetName);
         // 타겟 포착 실패시 메세지 호출 후 함수 종료
         if (target == null) {
-            Debug.Log("Error : Could not find object.");
+            Debug.Log("Basesystem.functionCaller.error : Could not find object.");
             return;
         }
         target.SendMessage(msg.functionName, msg);
         // Debug.Log(msg.returnValue[0]);
+    }
+
+    public void playInfoSetter(Message msg) {
+        string infoName = (string)msg.args[0];  // 변경 대상 이름.
+        object value = msg.args[1];             // 변경 값.
+        int index = findInfo(infoName);
+
+        if (index == -1) Debug.Log("BaseSystem/playInfoChanger.error : there is no information which name is " + infoName);
+        else playInfoList[index].setValue(value);
+    }
+    public void playInfoChanger(Message msg) {
+        string infoName = (string)msg.args[0];  // 변경 대상 이름.
+        object value = msg.args[1];             // 변경 값.
+        int index = findInfo(infoName);
+
+        if (index == -1) Debug.Log("BaseSystem/playInfoChanger.error : there is no information which name is " + infoName);
+        else playInfoList[index].modifyValue(value);
+    }
+
+    int findInfo(string infoName) {
+        int i;
+        for (i = 0; i < playInfoList.Count; ) {
+            if (playInfoList[i].getInfoName() == infoName) return i;
+            else i++;
+        }
+        
+        // 중간에 루프를 탈출하지 않았다면 못 찾은 것이므로 -1 반환.
+        return -1;
+    }
+
+    public void getPlayInfo(Message msg) {
+        string infoName = (string) msg.args[0]; // 정보를 원하는 대상 이름.
+        int index = findInfo(infoName);
+        if (index == -1) Debug.Log("BaseSystem/getPlayInfo.error : there is no information which name is " + infoName);
+        else {
+            msg.returnValue.Add(playInfoList[index].getInfoValue());    // 오브젝트 형으로 정보 반환.
+            msg.returnValue.Add(playInfoList[index].getInfoType());     // 언박싱 위한 타입
+        }
     }
 }
 
---
+++ project/Capstone_Design/Assets/BaseSystem/PlayInfo.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayInfo {
+    int INT_NONE = -999999;
+    float FLOAT_NONE = -9999.99f;
+
+    string infoName;
+    string infoType;
+    int infoValue_typeInt;
+    float infoValue_typeFloat;
+    string infoValue_typeString;
+
+
+    public PlayInfo(string infoName) {
+        this.infoName = infoName;
+        this.infoType = "none";
+        this.infoValue_typeInt = INT_NONE;
+        this.infoValue_typeFloat = FLOAT_NONE;
+        this.infoValue_typeString = "";
+    }
+    public PlayInfo(string infoName, int infoValue) : this(infoName) {
+        this.infoName = infoName;
+        this.infoType = "int";
+        this.infoValue_typeInt = infoValue;
+    }
+    public PlayInfo(string infoName, float infoValue) : this(infoName) {
+        this.infoName = infoName;
+        this.infoType = "float";
+        this.infoValue_typeFloat = infoValue;
+    }
+    public PlayInfo(string infoName, string infoValue) : this(infoName) {
+        this.infoName = infoName;
+        this.infoType = "string";
+        this.infoValue_typeString = infoValue;
+    }
+
+    public string getInfoName() {
+        return this.infoName;
+    }
+
+    public object getInfoValue() {
+        if (infoType == "int")          return infoValue_typeInt;
+        else if (infoType == "float")   return infoValue_typeFloat;
+        else if (infoType == "string")  return infoValue_typeString;
+        else                            return null;
+    }
+
+    public string getInfoType() {
+        return this.infoType;
+    }
+
+    public void setValue(object value) { 
+        if (infoType == "int")          this.infoValue_typeInt      = (int) value; 
+        else if (infoType == "float")   this.infoValue_typeFloat    = (float) value;
+        else if (infoType == "string")  this.infoValue_typeString   = (string) value;
+    }
+
+    public void modifyValue(object value) {
+        if (infoType == "int")          this.infoValue_typeInt      += (int) value; 
+        else if (infoType == "float")   this.infoValue_typeFloat    += (float) value;
+        else if (infoType == "string")  this.infoValue_typeString   += (string) value;        
+    }
+}
