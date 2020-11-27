9db471bac79aa53dc9328a96abf7ddc22685f95c
slient13 <sjyhc25@gmail.com>
2020-11-27 18:02:30 +0900

    UI 매니저 1차

---
+++ project/Capstone_Design/Assets/UI/UIManager.cs
@@ -0,0 +1,7 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+    public List<GameObject> UILayer = new List<GameObject>();    
+}
