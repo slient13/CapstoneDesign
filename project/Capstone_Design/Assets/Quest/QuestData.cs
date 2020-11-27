e81d0d3901072400c270727a079eaca8fe37fe14
ddrttl300 <ddrttl500@gmail.com>
2020-11-27 20:35:19 +0900

    ㅇㅇ
    ㅇ

---
+++ project/Capstone_Design/Assets/Quest/QuestData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+
+public class QuestData
+{
+    public string questName;
+    public int[] npcId;
+
+    public QuestData(string name, int[] npc)
+    {
+        questName = name;
+        npcId = npc;
+    }
+}
+
---
+++ project/Capstone_Design/Assets/Quest/QuestManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class QuestManager : MonoBehaviour
+{
+    public TalkManager talkManager;
+    public Text talkText;
+    public GameObject scanObject;
+    public GameObject talkPanel;
+    public bool isAction;
+    public int talkIndex;
+    
+
+    // Start is called before the first frame update
+    void Action(GameObject scanObj)
+
+    {
+            scanObject = scanObj;
+            ObjData objData = scanObject.GetComponent<ObjData>();
+            Talk(objData.id, objData.isNpc);
+        
+
+        talkPanel.SetActive(isAction);
+    }
+
+    // Update is called once per frame
+    void Talk(int id, bool isNpc)
+    {
+        string talkData = talkManager.GetTalk(id, talkIndex);
+        if(talkData == null)
+        {
+            isAction = false;
+            talkIndex = 0;
+            return;
+        }
+        if(isNpc)
+        {
+            talkText.text = talkData;
+        }
+        else
+        {
+            talkText.text = talkData;
+        }
+        isAction = true;
+        talkIndex++;
+    }
+}
