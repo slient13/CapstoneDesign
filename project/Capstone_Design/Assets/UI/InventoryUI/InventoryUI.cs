2f8c1a8f2c7def711aa537172b7a1e5b89553862
zzzxxxddd8 <80624656+zzzxxxddd8@users.noreply.github.com>
2021-05-20 18:48:21 +0900

    Upload

---
+++ project/Capstone_Design/Assets/UI/InventoryUI/InventoryUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    public GameObject InventoryPanel;
+    bool activeInventory = false;
+
+    private void Start()
+    {
+        InventoryPanel.SetActive(activeInventory);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            activeInventory = !activeInventory;
+            InventoryPanel.SetActive(activeInventory);
+        }
+    }
+}
