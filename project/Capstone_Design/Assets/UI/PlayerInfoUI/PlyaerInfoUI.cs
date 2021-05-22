6c1f322808da286603a06d9098a07324b7688f34
zzzxxxddd8 <80624656+zzzxxxddd8@users.noreply.github.com>
2021-05-22 20:56:42 +0900

    MenuUpdata

---
+++ project/Capstone_Design/Assets/UI/PlayerInfoUI/PlyaerInfoUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlyaerInfoUI : MonoBehaviour
+{
+    public GameObject infoPanel;
+    bool activeInfo = false;
+
+    private void Start()
+    {
+        infoPanel.SetActive(activeInfo);
+    }
+
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            activeInfo = !activeInfo;
+            infoPanel.SetActive(activeInfo);
+        }
+    }
+}
