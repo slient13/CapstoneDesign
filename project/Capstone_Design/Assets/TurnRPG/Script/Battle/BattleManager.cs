d63cc45bf9c734420d3a80d7a009dbff32a0a7f2
orangesnack <sacar1920@gmail.com>
2021-05-28 19:12:38 +0900

    UI추가
    작업중

---
+++ project/Capstone_Design/Assets/TurnRPG/Script/Battle/BattleManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleManager : MonoBehaviour
+{
+    public string[] mainComment;
+    public string monsterName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainComment = new string[] {"앗! 야생의 " + monsterName + "이 나타났다!",};
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
