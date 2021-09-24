f10f2328e842e5f0cf223a85ed9251b7c7df67d8
zzzxxxddd8 <80624656+zzzxxxddd8@users.noreply.github.com>
2021-09-24 17:39:42 +0900

    튜토리얼추가중

---
+++ project/Capstone_Design/Assets/TutorialScript/TNPC.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TNPC : MonoBehaviour
+{
+
+
+    private void OnTriggerEnter(Collider collider)
+
+    {
+        //Debug.Log("충돌 시작!");
+        Message msg = new Message("Guide");
+
+
+    }
+    // Collider 컴포넌트의 is Trigger가 false인 상태로 충돌이 끝났을 때
+
+    private void OnTriggerExit(Collider collider)
+
+    {
+
+        //Debug.Log("충돌 끝!");
+
+
+    }
+
+}
+
+
+
+
+
