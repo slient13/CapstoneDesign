5e06a8c0622eb5bd4ff42a61010a7f1bc4e76e5a
zzzxxxddd8 <80624656+zzzxxxddd8@users.noreply.github.com>
2021-09-27 18:16:26 +0900

    튜토리얼,로딩 틀제작

---
+++ project/Capstone_Design/Assets/TutorialScript/NPC_Trigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_Trigger : MonoBehaviour
+{
+    public string ChatText = "";
+    private GameObject Main;
+    void Start()
+    {
+        Main = GameObject.Find("Main");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Main.GetComponent<MainScript>().NPCChatEnter(ChatText);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Main.GetComponent<MainScript>().NPCChatExit();
+        }
+    }
+}
