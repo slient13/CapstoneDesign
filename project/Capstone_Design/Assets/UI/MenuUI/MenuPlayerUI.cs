3cb02a93b56d2bd077585bccbdb2200202baa9ee
slient13 <sjyhc25@gmail.com>
2021-11-09 22:52:43 +0900

    MenuPlayerUI 예외처리 추가

--- project/Capstone_Design/Assets/UI/MenuUI/MenuPlayerUI.cs
+++ project/Capstone_Design/Assets/UI/MenuUI/MenuPlayerUI.cs
@@ -22,30 +22,41 @@ public class MenuPlayerUI : MonoBehaviour
         EventListener.GetEventListener().Binding("BaseSystem", "ChangeData", "MenuUI/Sync : ");
         Sync();
     }
-    public void Sync() {
-        Message getHp = new Message("GetPlayInfo : Player.Stat.Hp").FunctionCall();
-        PlayInfo Hp = (PlayInfo) getHp.returnValue[0];
-        PlayInfo Hp_data = Hp.GetDataList()[0];
-        hpBar.Sync((int)Hp_data.GetValue(), (int)Hp_data.GetRange()[1]);
-        Message getStamina = new Message("GetPlayInfo : Player.Stat.Sp").FunctionCall();
-        PlayInfo Sp = (PlayInfo) getStamina.returnValue[0];
-        PlayInfo Sp_data = Sp.GetDataList()[0];
-        spBar.Sync((int)Sp_data.GetValue(), (int)Sp_data.GetRange()[1]);
+    public void Sync()
+    {
+        try
+        {
+            Message getHp = new Message("GetPlayInfo : Player.Stat.Hp").FunctionCall();
+            PlayInfo Hp = (PlayInfo)getHp.returnValue[0];
+            PlayInfo Hp_data = Hp.GetDataList()[0];
+            hpBar.Sync((int)Hp_data.GetValue(), (int)Hp_data.GetRange()[1]);
+            Message getStamina = new Message("GetPlayInfo : Player.Stat.Sp").FunctionCall();
+            PlayInfo Sp = (PlayInfo)getStamina.returnValue[0];
+            PlayInfo Sp_data = Sp.GetDataList()[0];
+            spBar.Sync((int)Sp_data.GetValue(), (int)Sp_data.GetRange()[1]);
+        }
+        catch
+        {
+            Debug.Log("MenuPlayerUI.Sync.error : there is no hp/sp stick");
+        }
     }
 }
 
-public class StatBar {
+public class StatBar
+{
     public string name;
     public int degree;
     public int max;
     public float width;
     public RectTransform panel;
     public Text text;
-    public StatBar(string name) {
+    public StatBar(string name)
+    {
         this.name = name;
-    }    
+    }
 
-    public void Sync(int degree, int max) {
+    public void Sync(int degree, int max)
+    {
         this.degree = degree;
         this.max = max;
         panel.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, degree * width / 100);
